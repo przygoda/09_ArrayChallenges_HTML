8ca32a7cfd46ead672fa32be92c2c5663fe64447
Peter <schmidt.multimedia@gmail.com>
2020-09-25 10:07:14 +0200

    update 3

--- challenge_arr_02.js
+++ challenge_arr_02.js
@@ -1,3 +1,28 @@
 
 /* Vorüberlegungen */
 // push() / pop()
+
+/*
+// push --> Daten rein ... (+)
+let arr = [];
+ausgabe(arr);
+arr.push("Ich");
+ausgabe(arr);
+arr.push("bin");
+ausgabe(arr);
+arr.push("Peter");
+ausgabe(arr);
+
+// pop() --> Daten raus ... (-)
+ausgabe(arr.pop());
+ausgabe(arr);
+ausgabe(arr.pop());
+ausgabe(arr);
+ausgabe(arr.pop());
+ausgabe(arr);
+
+//ausgabe("hi");
+function ausgabe(outputStr) {
+    console.log(outputStr);
+}
+*/
