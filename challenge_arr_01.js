254ec565d42c2aa1f10ac2a7f59af27c21e2deee
Peter <schmidt.multimedia@gmail.com>
2020-09-25 10:07:00 +0200

    update 3

--- challenge_arr_01.js
+++ challenge_arr_01.js
@@ -10,15 +10,17 @@ Verwenden Sie dafür die untenstehenden Arrays
 
 let controls = ["<",">","/"];
 let tags = ["html","head","head","body","h1","h1","p","p","p","p","body","html"];
- // Ziel --> "<html><head></head><body><p></p></body></html>"; 
+let stack = [];
 
+
+ // Ziel --> "<html><head></head><body><p></p></body></html>"; 
 ausgabe(getHTML());
 function getHTML() {
 
     let htmlStr = "";
 
     for (let i = 0; i < tags.length; i++) {
-        if (isOpenTag(i)) {// ???
+        if (isOpenTagStack(i)) {// ???
             htmlStr += getTag(tags[i],"open");
         } else {
             htmlStr += getTag(tags[i],"close");
@@ -28,11 +30,38 @@ function getHTML() {
     return htmlStr;
 }
 
+/*
+ausgabe(isOpenTagStack(0));
+ausgabe(isOpenTagStack(1));
+ausgabe(isOpenTagStack(2));*/
+function isOpenTagStack(index) {
+
+    // Ist das Element neu ?? // dann open und in den Stack
+    // Wenn nicht // dann close und raus aus dem Stack
+
+    let element = tags[index];
+    
+   // ausgabe(element + " : " + stack.indexOf(element));
+    
+    let cond    = (stack.indexOf(element) == -1); // element neu??
+
+    if (cond) {  // Element neu ... --> rein, open(true)
+        stack.push(element);
+        ausgabe(stack);
+        return true;
+    } else { // Element nicht neu --> raus, close(false)
+        stack.pop();
+        ausgabe(stack);
+        return false;
+    }
+}
+
+
 // Modul: // erscheint das tag zum ersten mal ? --> true/false
 /*ausgabe(isOpenTag(0));
 ausgabe(isOpenTag(1));
 ausgabe(isOpenTag(2));*/
-function isOpenTag(index) {
+function isOpenTagIndex(index) {
    //return index + " : " + tags.indexOf(tags[index]); // Vorüberlegung
    return tags.indexOf(tags[index]) == index;
 }
