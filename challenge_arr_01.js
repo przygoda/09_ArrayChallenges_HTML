bd72f4e86074e83ad57356fd019bcf7810077795
Peter <schmidt.multimedia@gmail.com>
2020-09-28 15:11:02 +0200

    update TABs

--- challenge_arr_01.js
+++ challenge_arr_01.js
@@ -8,10 +8,10 @@ einer HTML-Seite ausgibt:
 Verwenden Sie dafür die untenstehenden Arrays
 */
 
-let controls = ["<",">","/"];
-let tags = ["html","head","head","body","h1","h1","p","p","p","p","body","html"];
+let controls = {open_o:"<", open_cl:"</", close:">"};
+let tags = ["html","head","head","body","h1","h1","p","p","ul","li","li","ul","body","html"];
 let stack = [];
-
+let tabCount = 0;
 
  // Ziel --> "<html><head></head><body><p></p></body></html>"; 
 ausgabe(getHTML());
@@ -21,16 +21,40 @@ function getHTML() {
 
     for (let i = 0; i < tags.length; i++) {
         if (isOpenTagStack(i)) {// ???
-            htmlStr += getTag(tags[i],"open");
+            htmlStr += getTabs() + getTag(tags[i],"open") + "\n";
         } else {
-            htmlStr += getTag(tags[i],"close");
+            htmlStr += getTabs() + getTag(tags[i],"close") + "\n";;
         }
     }
     
     return htmlStr;
 }
 
-/*
+/* // Modul: Anzahle der Tabs: |Tests: */
+function getTabs() {
+    let tabStr = "";
+    for (let i = 0; i < tabCount; i++) {
+       tabStr += "\t";
+    }
+    return tabStr;
+}
+
+/* // Modul: Zusammenbau: <tagStr> --> Tests:
+ausgabe(getTag("p","open"));
+ausgabe(getTag("p","close"));
+ausgabe(getTag("p","jkjlkjlkjl")); */
+function getTag(tagName,flag) { // <p> | </p> 
+    switch (flag) {
+        case "open":
+            return controls.open_o + tagName + controls.close;     
+        case "close":
+            return controls.open_cl + tagName + controls.close;
+            default:
+            return "#";
+    }
+}
+
+/* // Modul: // Stack :: erscheint das tag zum ersten mal ? --> true/false/
 ausgabe(isOpenTagStack(0));
 ausgabe(isOpenTagStack(1));
 ausgabe(isOpenTagStack(2));*/
@@ -46,39 +70,25 @@ function isOpenTagStack(index) {
     let cond    = (stack.indexOf(element) == -1); // element neu??
 
     if (cond) {  // Element neu ... --> rein, open(true)
+        //ausgabe(stack.length);
+        tabCount = stack.length
         stack.push(element);
-        ausgabe(stack);
         return true;
     } else { // Element nicht neu --> raus, close(false)
         stack.pop();
-        ausgabe(stack);
+        //ausgabe(stack.length);
+        tabCount = stack.length
         return false;
     }
 }
 
-
-// Modul: // erscheint das tag zum ersten mal ? --> true/false
-/*ausgabe(isOpenTag(0));
+/* // Modul: // Index :: erscheint das tag zum ersten mal ? --> true/false
+ausgabe(isOpenTag(0));
 ausgabe(isOpenTag(1));
 ausgabe(isOpenTag(2));*/
 function isOpenTagIndex(index) {
    //return index + " : " + tags.indexOf(tags[index]); // Vorüberlegung
    return tags.indexOf(tags[index]) == index;
-}
-
-// Modul: Zusammenbau: <tagStr> --> Tests:
-/* ausgabe(getTag("p","open"));
-ausgabe(getTag("p","close"));
-ausgabe(getTag("p","jkjlkjlkjl")); */
-function getTag(tagName,flag) { // <p> | </p> 
-    switch (flag) {
-        case "open":
-            return controls[0] + tagName + controls[1];     
-        case "close":
-            return controls[0] + controls[2] + tagName + controls[1];
-            default:
-            return "#";
-    }
 }
 
 //ausgabe("hi");
